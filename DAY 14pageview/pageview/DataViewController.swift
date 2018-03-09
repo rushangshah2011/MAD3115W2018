bf4e9debf38208cee270cd31f14832a0a38be521
rushangshah2011 <35268283+rushangshah2011@users.noreply.github.com>
2018-03-09 11:16:10 -0500

    Add files via upload

---
+++ DAY 14pageview/pageview/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  pageview
+//
+//  Created by MacStudent on 2018-03-09.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
---
+++ DAY 14pageview/pageview/TutorialPageViewController.swift
@@ -0,0 +1,96 @@
+//
+//  TutorialPageViewController.swift
+//  pageview
+//
+//  Created by MacStudent on 2018-03-09.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class TutorialPageViewController: UIPageViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        dataSource = self
+        
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController],
+                               direction: .forward,
+                               animated: true,
+                               completion: nil)
+        }
+    }
+    
+    
+    
+    private(set) lazy var orderedViewControllers: [UIViewController] = {
+        return [self.newColoredViewController(color: "red"),
+                self.newColoredViewController(color: "green"),
+                self.newColoredViewController(color: "blue")]
+    }()
+    
+    private func newColoredViewController(color: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil) .
+            instantiateViewController(withIdentifier: "\(color)")
+    }
+    
+    
+}
+
+// MARK: UIPageViewControllerDataSource
+
+
+extension TutorialPageViewController: UIPageViewControllerDataSource {
+    
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        // User is on the first view controller and swiped left to loop to
+        // the last view controller.
+        guard previousIndex >= 0 else {
+            return orderedViewControllers.last
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let orderedViewControllersCount = orderedViewControllers.count
+        
+        // User is on the last view controller and swiped right to loop to
+        // the first view controller.
+        guard orderedViewControllersCount != nextIndex else {
+            return orderedViewControllers.first
+        }
+        
+        guard orderedViewControllersCount > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
+
+    }
+
+
+    
+
+
+
+}
